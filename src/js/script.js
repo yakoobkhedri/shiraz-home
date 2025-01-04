c41def32d74a7106b8c4c4f1a30c7b96f696adaf
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-01-04 21:24:26 +0330

    commit message

--- src/js/script.js
+++ src/js/script.js
@@ -46,7 +46,22 @@ var category = new Swiper(".category", {
     }
   },
 });
-
+var homes = new Swiper(".homes", {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  // autoplay: true,
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    }
+  },
+});
 
 // select size
 
