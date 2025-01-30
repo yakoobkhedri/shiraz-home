bc51b3d6b03e10e76e0001ac8766dad3436ae924
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-01-30 18:36:17 +0330

    commit message

--- src/js/script.js
+++ src/js/script.js
@@ -292,3 +292,14 @@ acordionBtn3.forEach((item) => {
     item.querySelector('svg').classList.toggle('active');
   })
 })
+
+// checkbox
+
+let checkbox = Array.from(document.getElementsByClassName('checkbox'));
+let checkboxRow = Array.from(document.getElementsByClassName('checkboxRow'));
+
+checkboxRow.forEach((item) => {
+    item.addEventListener('click', function () {
+        item.querySelector('.checkbox').classList.toggle('active');
+    })
+})
